0bd1c5c485cbac7752f45fd1cb1a2214417c8bc4
shark-andrey <163378908+shark-andrey@users.noreply.github.com>
2024-06-24 08:05:15 +0500

    Update record.py

--- src/record.py
+++ src/record.py
@@ -15,7 +15,7 @@ class Record(typing.NamedTuple):
     event_number: int
     round_number: int
     heat_number: int
-    place: str | None
+    place: int | None
     athlete_id: int
     lane: int | None
     time: Decimal | None
