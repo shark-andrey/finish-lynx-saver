8189332b3a54a01e5fbb75388f9665d20810f8e4
shark-andrey <163378908+shark-andrey@users.noreply.github.com>
2025-06-22 22:10:38 +0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-port = os.getenv("PORT", 9955)
+port = os.getenv("PORT", 7755)
 db_url = os.getenv("DB_URL", "mysql+asyncmy://root:password@localhost:3306/dev")
 # db_url = os.getenv("DB_URL", "mysql+pymysql://root:password@localhost:3306/dev")
 table_name = "finish_lynx"
