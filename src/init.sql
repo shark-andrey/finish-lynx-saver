86c3804aba83c8ef372ef0068c1ee3aeb54b2e8f
shark-andrey <163378908+shark-andrey@users.noreply.github.com>
2026-01-28 22:06:40 +0500

    Update init.sql

--- src/init.sql
+++ src/init.sql
@@ -4,7 +4,7 @@ create table if not exists TABLE_NAME (
     heat_number int not null,
     place int,
     athlete_id int not null,
-    lane int,
+    lane varchar(255),
     time decimal(7, 2),
     react_time decimal(7, 2),
     wind varchar(255),
